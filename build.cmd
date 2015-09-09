7aa7f0842a47618fd8139c4d3d5c662ce14df63c
Daniel Steuernol <dlsteuer@gmail.com>
2015-09-08 19:47:29 -0700

    added nuspec

--- build.cmd
+++ build.cmd
@@ -1,6 +1,13 @@
 @echo off
 cls
 if not exist tools mkdir tools
-@powershell "wget https://nuget.org/nuget.exe -OutFile tools\Nuget.exe"
+if not exist tools\Nuget.exe @powershell "wget https://nuget.org/nuget.exe -OutFile tools\Nuget.exe"
 "tools\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
 "packages\FAKE\tools\Fake.exe" build.fsx Target=%1 %*
+
+set nuget=
+if "%nuget%" == "" (
+    set nuget=nuget
+)
+
+%nuget% pack "RazorContentChecker.nuspec" -NoPackageAnalysis -OutputDirectory $buildArtifactsDirectory
